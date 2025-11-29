90b33315ff9bbad3761a3a453d9b46fe5e53cf39
Atul-khadoliya <85124877+Atul-khadoliya@users.noreply.github.com>
2025-11-30 01:11:20 +0530

    Update Program-1.cpp

--- Program-1.cpp
+++ Program-1.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Calculator {
+public:
+    double a, b;
+    string op;
+
+    Calculator(double x, double y, string type) {
+        a = x;
+        b = y;
+        op = type;
+    }
+
+    double calculate() {
+        if (op == "add") return a + b;
+        else if (op == "sub") return a - b;
+        else if (op == "mul") return a * b;
+        else if (op == "div") {
+            if (b == 0) {
+                cout << "Error: division by zero!\n";
+                return 0;
+            }
+            return a / b;
+        }
+        else {
+            cout << "Invalid operation!";
+            return 0;
+        }
+    }
+};
+
+int main() {
+    Calculator c(10.5, 5.5, "add");
+    cout << "Result: " << c.calculate();
+    return 0;
+}
 
